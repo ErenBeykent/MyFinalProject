e0cf88204436a2becbdb7618583fcafedacc1570
ErenBeykent <erengozsuz28@gmail.com>
2021-10-16 10:33:28 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepository<T>
+    {
+        List<T> GetAll(Expression<Func<T,bool>> filter=null); //Filtreleme yapmak için(GetAll'ın içi)
+        T Get(Expression<Func<T, bool>> filter); //Tek bir şey filtreleme için
+        void Add(T entity);
+        void Uptade(T entity);
+        void Delete(T entity);
+    }
+}
